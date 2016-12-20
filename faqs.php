5a8d39494fb66efb434aad0878b2f69b101e42da
sara-h-chen <sara.h.chen@durham.ac.uk>
2016-12-20 17:32:22 +0000

    Finish search functionality

--- faqs.php
+++ faqs.php
@@ -1,47 +1,71 @@
 <?php
 
-/**
- * gets the HTTP method, path and body of the request
- */
+    /**
+     * gets the HTTP method, path and body of the request
+     */
     $method = $_SERVER['REQUEST_METHOD'];
     $request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
-    // if you are trying to upload a file to the webserver
+    // Uncomment if you want to upload a file to the webserver
     //$input = json_decode(file_get_contents('php://input'), true);
     
     $link = new PDO('sqlite:./data/topics.db') or die("Failed to open the database");
+    // Get the list of topics
     $result = $link->query("SELECT DISTINCT topic FROM faqs");
     $item = $result->fetchAll(PDO::FETCH_COLUMN);
     $topicList = array('topics'=>$item);
 
-    $result = $link->query("SELECT question,answer FROM faqs");
-    //$itemize = $result->fetchAll(PDO::FETCH_ASSOC);
-    //$output = array('faqs'=>$itemize);
-
     header('Content-Type: application/json');
-    // Allows the topics to be addressed by their indices, $x
     $topic = $_GET['topic'];
-    if ($topic == NULL) {
+    $q = $_GET['q'];
+    $ignoredWords = array('if', 'the', 'has', 'that', 'it', 'for', 'or', 'was', 'are', 'a', 'to');
+    $q = str_replace($ignoredWords, '', $q);
+
+    if ($topic == NULL && $q == NULL) {
+       // Return unfiltered data
        $result = $link->query("SELECT * FROM faqs");
        $itemize = $result->fetchAll(PDO::FETCH_ASSOC);
        $output = array('faqs'=>$itemize);
        echo json_encode($output);
-    } else { 
+
+    } else if ($topic !== NULL && $q == NULL) { 
+       // Find index of topic
        foreach ($topicList as $row) {
           $topic = $row[$topic];
        }
-
+       // Search database for topic, based on the index
        $result = $link->prepare("SELECT question,answer FROM faqs WHERE topic=?");
        $result->execute(array($topic));
        $itemize = $result->fetchAll(PDO::FETCH_ASSOC);
        $output = array('faqs'=>$itemize);
        echo json_encode($output);
-    }
 
-    $q = $_GET['q'];
-       $result = $link->prepare("SELECT question,answer FROM faqs WHERE question LIKE '%?%' OR answer LIKE '%?%' OR topic LIKE '%?%'");
-       $result->execute(array($q));
+    } else if ($topic == NULL && $q !== NULL) {
+       // Full text search
+       $result = $link->prepare("SELECT * FROM faqs WHERE topic LIKE :param OR answer LIKE :param OR question LIKE :param");
+       $result->bindValue(':param', '%' . $q . '%', PDO::PARAM_STR);
+       $result->execute();
        $itemize = $result->fetchAll(PDO::FETCH_ASSOC);
        $output = array('faqs'=>$itemize);
        echo json_encode($output);
 
+    } else {
+       // Full text search
+       $result = $link->prepare("SELECT * FROM faqs WHERE topic LIKE :param OR answer LIKE :param OR question LIKE :param");
+       $result->bindValue(':param', '%' . $q . '%', PDO::PARAM_STR);
+       $result->execute();
+       $itemize = $result->fetchAll(PDO::FETCH_ASSOC);
+
+       // Get index of topic
+       foreach ($topicList as $row) {
+          $topic = $row[$topic];
+       }
+       // Break up the array; find the 'topic' field for comparison
+       foreach ($itemize as $val) {
+          foreach ($val as $key2 => $val2) {
+              if ($val2 == $topic) {
+                  echo json_encode($val);
+              }
+          }
+       }
+    }
 
