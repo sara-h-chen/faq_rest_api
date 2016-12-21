506d62ad37ee3b14560444b90b5ef37b7e429d92
sara-h-chen <sara.h.chen@durham.ac.uk>
2016-12-21 21:19:50 +0000

    Begin UI

--- assets/custom.js
+++ assets/custom.js
@@ -1,16 +1,39 @@
 $(document).ready(function() {
 
-/*====================================
-BACKDROP SLIDESHOW SCRIPTS
-======================================*/
-$.vegas('slideshow', {
-backgrounds: [
-{ src: 'assets/img/1.jpg', fade: 1000, delay: 9000 },
-{ src: 'assets/img/2.jpg', fade: 1000, delay: 9000 },
-]
-})('overlay', {
-/** SLIDESHOW OVERLAY IMAGE **/
-src: 'assets/img/06.png'
+/*===============================
+BACKGROUND 3D STYLE
+=================================*/
+
+var lFollowX = 0,
+    lFollowY = 0,
+    x = 0,
+    y = 0,
+    friction = 1 / 30;
+
+function moveBackground() {
+
+  x += (lFollowX - x) * friction;
+  y += (lFollowY - y) * friction;
+
+  translate = 'translate(' + x + 'px, ' + y + 'px) scale(1.1)';
+
+  $('.bg').css({
+    '-webit-transform': translate,
+    '-moz-transform': translate,
+    'transform': translate
+  });
+
+  //window.request<a href="http://www.jqueryscript.net/animation/">Animation</a>Frame(moveBackground);
+}
+
+$(window).on('mousemove click', function(e) {
+  var lMouseX = Math.max(-100, Math.min(100, $(window).width() / 2 - e.clientX));
+  var lMouseY = Math.max(-100, Math.min(100, $(window).height() / 2 - e.clientY));
+  lFollowX = (20 * lMouseX) / 100; // 100 : 12 = lMouxeX : lFollow
+  lFollowY = (10 * lMouseY) / 100;
+});
+
+moveBackground();
 
 /*====================================
 FILTER FUNCTIONALITY SCRIPTS
