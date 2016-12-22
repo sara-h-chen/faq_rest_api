6688d155e2ba1f63d1dbdbab438f6c82f07830a4
sara-h-chen <deutscher71112@gmail.com>
2016-12-22 01:25:48 +0000

    Try 3D background

--- assets/custom.js
+++ assets/custom.js
@@ -4,42 +4,19 @@ $(document).ready(function() {
 BACKGROUND 3D STYLE
 =================================*/
 
-var lFollowX = 0,
-    lFollowY = 0,
-    x = 0,
-    y = 0,
-    friction = 1 / 30;
-
-function moveBackground() {
-
-  x += (lFollowX - x) * friction;
-  y += (lFollowY - y) * friction;
-
-  translate = 'translate(' + x + 'px, ' + y + 'px) scale(1.1)';
-
-  $('.bg').css({
-    '-webit-transform': translate,
-    '-moz-transform': translate,
-    'transform': translate
-  });
-
-  //window.request<a href="http://www.jqueryscript.net/animation/">Animation</a>Frame(moveBackground);
-}
-
-$(window).on('mousemove click', function(e) {
-  var lMouseX = Math.max(-100, Math.min(100, $(window).width() / 2 - e.clientX));
-  var lMouseY = Math.max(-100, Math.min(100, $(window).height() / 2 - e.clientY));
-  lFollowX = (20 * lMouseX) / 100; // 100 : 12 = lMouxeX : lFollow
-  lFollowY = (10 * lMouseY) / 100;
+$(".bg").interactive_bg({
+    strength: 25,
+    scale: 1.00,
+    animationSpeed: "120ms",
+    contain: true,
+    wrapContent: false
 });
-
-moveBackground();
 
 /*====================================
 FILTER FUNCTIONALITY SCRIPTS
 ======================================*/
 // FIX TO WORK ON BUTTON ITSELF
-$(window).load(function () {
+/*$(window).load(function () {
 var $container = $('#topics-div');
 $container.isotope({
 filter: '*',
@@ -66,5 +43,5 @@ return false;
 
 });
 
-
+*/
 });
