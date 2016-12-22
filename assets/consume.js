871a7c9e47ea37d0240060f255e8f7661cd586d3
sara-h-chen <deutscher71112@gmail.com>
2016-12-22 20:25:37 +0000

    Begin restructuring JSON parse

--- assets/consume.js
+++ assets/consume.js
@@ -1,9 +1,25 @@
 $(document).ready(function() {
     $.ajax({
-        url: "http://community.dur.ac.uk/sara.h.chen/faq2016/topics.php"
+        url: "http://community.dur.ac.uk/sara.h.chen/faq2016/faqs.php"
     }).then(function(data) {
-       var container = document.createElement('div');
-       container.innerHTML = '<div class="col-sm-6 col-md-3 topic"><a href="#" class="thumbnail">' + data.topics.join('</a></div><div class="col-sm-6 col-md-3 topic"><a href="#" class="thumbnail">') + '</a></div>';
-       document.body.appendChild(container);
+        /** CREATE ID FOR FILTERING TOPICS **/
+        var topics = [];
+        /* <a href="#" data-filter=".priv" class="btn btn-custom btn-custom-two btn-sm">Privacy and Surveillance</a> */
+       console.log(data.faqs);
+       for (var i=0; i <= data.faqs.length; i++) {
+           if (topics.includes((data.faqs[i]).topic)) {
+               continue;
+           } else {
+               var container = document.createElement('a');
+               //console.log("pushed");
+               topics.push(data.faqs[i].topic);
+               container.innerHTML = '<a href="#" data-filter=".' + data.faqs[i].topic.toLowerCase() + '" class="btn btn-custom btn-custom-two btn-sm">' + data.faqs[i].topic + '</a>';
+               document.getElementById("listOfTopics").appendChild(container);
+           };
+       }
+       // topics now contains the array of topics
+        // var container = document.createElement('div');
+       // container.innerHTML = '<div class="col-sm-6 col-md-3 topic" onclick="disappearing()"><a class="thumbnail">' + data.topics.join('</a></div><div class="col-sm-6 col-md-3 topic" onclick="disappearing()"><a class="thumbnail">') + '</a></div>';
+       // document.getElementById("topicList").appendChild(container);
     });
 });
