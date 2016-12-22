e8c7c35ee3c3de78a73bc4a5f906ab9bb7555882
sara-h-chen <deutscher71112@gmail.com>
2016-12-22 00:27:14 +0000

    Begin styling

--- assets/consume.js
+++ assets/consume.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
         url: "http://community.dur.ac.uk/sara.h.chen/faq2016/topics.php"
     }).then(function(data) {
        var container = document.createElement('div');
-       container.innerHTML = '<div>' + data.topics.join('</div><div>') + '</div>';
+       container.innerHTML = '<div class="col-sm-6 col-md-3"><a href="#" class="thumbnail">' + data.topics.join('</a></div><div class="col-sm-6 col-md-3"><a href="#" class="thumbnail">') + '</a></div>';
        document.body.appendChild(container);
     });
 });
