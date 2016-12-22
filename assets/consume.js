fa037a83838df17bf227252ba58bcc789893dbb3
sara-h-chen <deutscher71112@gmail.com>
2016-12-22 11:18:31 +0000

    Begin JS filter functionality

--- assets/consume.js
+++ assets/consume.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
         url: "http://community.dur.ac.uk/sara.h.chen/faq2016/topics.php"
     }).then(function(data) {
        var container = document.createElement('div');
-       container.innerHTML = '<div class="col-sm-6 col-md-3 vcenter"><a href="#" class="thumbnail">' + data.topics.join('</a></div><div class="col-sm-6 col-md-3 vcenter"><a href="#" class="thumbnail">') + '</a></div>';
+       container.innerHTML = '<div class="col-sm-6 col-md-3 topic"><a href="#" class="thumbnail">' + data.topics.join('</a></div><div class="col-sm-6 col-md-3 topic"><a href="#" class="thumbnail">') + '</a></div>';
        document.body.appendChild(container);
     });
 });
--- assets/custom.js
+++ assets/custom.js
@@ -3,33 +3,8 @@ $(document).ready(function() {
 /*====================================
 FILTER FUNCTIONALITY SCRIPTS
 ======================================*/
-// FIX TO WORK ON BUTTON ITSELF
-/*$(window).load(function () {
-var $container = $('#topics-div');
-$container.isotope({
-filter: '*',
-animationOptions: {
-duration: 750,
-easing: 'linear',
-queue: false
-}
-});
-$('.categories a').click(function () {
-$('.categories .active').removeClass('active');
-$(this).addClass('active');
-var selector = $(this).attr('data-filter');
-$container.isotope({
-filter: selector,
-animationOptions: {
-duration: 750,
-easing: 'linear',
-queue: false
-}
-});
-return false;
+$(".topic").click(function() {
+    $(".topic").not(this).hide();
 });
 
-});
-
-*/
 });
