7f193325e8fb7204a743d8e44669915dc22dfe28
sara-h-chen <deutscher71112@gmail.com>
2016-12-23 11:36:19 +0000

    Fix Javascript conflicts

--- assets/consume.js
+++ assets/consume.js
@@ -30,5 +30,31 @@ $(document).ready(function() {
        }
 
 
+    // }).then(function() {
+    //     var $container = $('#topics-div');
+    //     $container.isotope({
+    //         filter: '*',
+    //         animationOptions: {
+    //             duration: 750,
+    //             easing: 'linear',
+    //             queue: false
+    //         }
+    //     });
+    //
+    //     $('.categories a').click(function () {
+    //         $('.categories .active').removeClass('active');
+    //         $(this).addClass('active');
+    //         var selector = $(this).attr('data-filter');
+    //         $container.isotope({
+    //             filter: selector,
+    //             animationOptions: {
+    //                 duration: 750,
+    //                 easing: 'linear',
+    //                 queue: false
+    //             }
+    //         });
+    //         return false;
+    //     });
     });
+
 });
