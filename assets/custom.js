349901a5a0b16e33ddf8eeea33f45b908f791086
sara-h-chen <sara.h.chen@durham.ac.uk>
2016-12-24 12:16:17 +0000

    Update to test locally

--- assets/custom.js
+++ assets/custom.js
@@ -36,6 +36,7 @@ $(document).ready(function () {
             },
             error: function(jqXHR, textStatus, errorThrown) {
                 console.log("failed");
+                console.log(textStatus + errorThrown);
                 $('[data-remodal-id=FailedModal]').remodal().open();
             }
         });
